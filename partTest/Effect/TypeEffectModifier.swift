d696cc3aa6e2ffef4219c5b1cd3053849a3f4c79
valselee <sollleky72@gmail.com>
2023-03-29 20:46:41 +0900

    [Ad d] Type Effect Modifier, View Extenstion 구현

---
+++ partTest/Effect/TypeEffectModifier.swift
@@ -0,0 +1,20 @@
+//
+//  TypeEffectModifier.swift
+//  partTest
+//
+//  Created by Celan on 2023/03/29.
+//
+
+import SwiftUI
+
+struct TypeEffectModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TypeEffectModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        TypeEffectModifier()
+    }
+}
